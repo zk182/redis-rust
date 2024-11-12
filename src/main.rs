31587f2243d14ab3fe863f87168b9ce250109544
Nicolas Battaglia <nicobattmdq@gmail.com>
2024-11-12 11:38:01 +0100

    Dale isabel!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use tokio::net::TcpListener;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use std::collections::HashMap;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -7,6 +8,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let (mut socket, _) = listener.accept().await?;
+        let mut map= HashMap::new();
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
@@ -17,7 +19,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => {
-                        let result= redis_protocol_parser(&buf[..n]);
+                        let result= compute_command(&buf[..n], &mut map);
                         let format_result = format!("+{}",{result});
                         socket.write(format_result.as_bytes()).await.expect("Error");
                     },
@@ -31,16 +33,36 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 }
 
-fn redis_protocol_parser(data: &[u8]) -> String {
-    let data_str = std::str::from_utf8(data).expect("Error parsing from utf8");
-    let binding: String = data_str.to_lowercase();
+fn compute_command(data: &[u8], map: &mut HashMap<String,String>) -> String{
+    let data = std::str::from_utf8(data).expect("Error parsing from utf8");
+    let binding: String = data.to_lowercase();
     let lines: Vec<&str> = binding.split("\r\n").collect();
     let command = lines[2];
-    
-    if command != "echo" {
-        return "PONG\r\n".to_string();
-    }
 
-    let arg = lines[4];
-    return arg.to_string()+ "\r\n";
+    match command {
+        "echo" => redis_protocol_parser(lines),
+        "set" => set_protocol_parser(lines, map),
+        "get" => get_protocol_parser(lines, map),
+        _ =>  "PONG\r\n".to_string(),
+    }
+}
+
+fn set_protocol_parser(data: Vec<&str>, map: &mut HashMap<String,String>) -> String {
+    let key = data[4].to_string();
+    let value = data[6].to_string();
+
+    println!("Setting key {}, value {}", key, value);
+
+    map.insert(key, value);
+    return "OK\r\n".to_string();
+}
+
+fn get_protocol_parser(data: Vec<&str>, map: &mut HashMap<String,String>) -> String {
+    let key = data[4].to_string();
+    let value = map.get(&key).cloned().unwrap_or("valor por defecto".to_string()) + "\r\n";
+    return value;
+}
+
+fn redis_protocol_parser(data: Vec<&str>) -> String {
+    return data[4].to_string() + "\r\n";
 }
