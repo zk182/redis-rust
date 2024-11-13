37bd78f6660709b18ce1d06671bf8dbbc37f96ce
Nicolas Battaglia <nicobattmdq@gmail.com>
2024-11-13 13:42:24 +0100

    CLI tests (cli-test-1731501744071)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
-use tokio::net::TcpListener;
+use command_parser::CommandParser;
+use storage::Storage;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use std::collections::HashMap;
+use tokio::net::TcpListener;
+
+mod command_parser;
+mod storage;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -8,10 +12,11 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     loop {
         let (mut socket, _) = listener.accept().await?;
-        let mut map= HashMap::new();
 
         tokio::spawn(async move {
             let mut buf = [0; 1024];
+            let mut storage = Storage::new();
+            // let command_parser = CommandParser::new();
 
             // In a loop, read data from the socket and write the data back.
             loop {
@@ -19,10 +24,9 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     // socket closed
                     Ok(n) if n == 0 => return,
                     Ok(n) => {
-                        let result= compute_command(&buf[..n], &mut map);
-                        let format_result = format!("+{}",{result});
-                        socket.write(format_result.as_bytes()).await.expect("Error");
-                    },
+                        let result = CommandParser::compute_command(&buf[..n], &mut storage);
+                        socket.write(result.as_bytes()).await.expect("Error");
+                    }
                     Err(e) => {
                         eprintln!("failed to read from socket; err = {:?}", e);
                         return;
@@ -31,38 +35,4 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }
         });
     }
-}
-
-fn compute_command(data: &[u8], map: &mut HashMap<String,String>) -> String{
-    let data = std::str::from_utf8(data).expect("Error parsing from utf8");
-    let binding: String = data.to_lowercase();
-    let lines: Vec<&str> = binding.split("\r\n").collect();
-    let command = lines[2];
-
-    match command {
-        "echo" => redis_protocol_parser(lines),
-        "set" => set_protocol_parser(lines, map),
-        "get" => get_protocol_parser(lines, map),
-        _ =>  "PONG\r\n".to_string(),
-    }
-}
-
-fn set_protocol_parser(data: Vec<&str>, map: &mut HashMap<String,String>) -> String {
-    let key = data[4].to_string();
-    let value = data[6].to_string();
-
-    println!("Setting key {}, value {}", key, value);
-
-    map.insert(key, value);
-    return "OK\r\n".to_string();
-}
-
-fn get_protocol_parser(data: Vec<&str>, map: &mut HashMap<String,String>) -> String {
-    let key = data[4].to_string();
-    let value = map.get(&key).cloned().unwrap_or("valor por defecto".to_string()) + "\r\n";
-    return value;
-}
-
-fn redis_protocol_parser(data: Vec<&str>) -> String {
-    return data[4].to_string() + "\r\n";
 }
---
+++ src/storage.rs
@@ -0,0 +1,61 @@
+use std::collections::HashMap;
+use std::time::Instant;
+
+#[derive(Debug)]
+pub struct Item {
+    pub value: String,
+    pub created: Instant,
+    pub expires: usize,
+}
+
+pub struct Storage {
+    pub storage: HashMap<String, Item>,
+}
+
+impl Storage {
+    pub fn new() -> Self {
+        Storage {
+            storage: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: &str, value: &str, expires: usize) {
+        let item = Item {
+            value: String::from(value),
+            expires,
+            created: Instant::now(),
+        };
+        println!("Item con key {:?} esta por setearse: {:?}", key, item);
+        self.storage.insert(key.to_string(), item);
+    }
+
+    pub fn get(&self, key: &str) -> Option<&Item> {
+        let item = self.storage.get(key)?;
+        let is_expired =
+            item.expires > 0 && item.created.elapsed().as_millis() > item.expires as u128;
+        println!(
+            "Getting key: {:?}, value: {:?}, is_expired : {:?},",
+            key, item.value, is_expired
+        );
+        match is_expired {
+            true => None,
+            false => Some(item),
+        }
+    }
+}
+
+impl Default for Item {
+    fn default() -> Self {
+        Item {
+            value: String::new(),
+            created: Instant::now(),
+            expires: 0,
+        }
+    }
+}
+
+impl Default for Storage {
+    fn default() -> Self {
+        Storage::new()
+    }
+}
