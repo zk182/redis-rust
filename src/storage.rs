0f56fd1e3e5829464b29573e0da8b5bf1ac41a71
Nicolas Battaglia <nicobattmdq@gmail.com>
2024-11-13 20:00:04 +0100

    CLI tests (cli-test-1731524404906)

--- src/storage.rs
+++ src/storage.rs
@@ -1,4 +1,5 @@
 use std::collections::HashMap;
+use std::env;
 use std::time::Instant;
 
 #[derive(Debug)]
@@ -8,14 +9,42 @@ pub struct Item {
     pub expires: usize,
 }
 
+pub struct Config {
+    pub dir: String,
+    pub dbfilename: String,
+}
+
 pub struct Storage {
     pub storage: HashMap<String, Item>,
+    pub config: Config,
 }
 
 impl Storage {
     pub fn new() -> Self {
+
+        //let args:Vec<_> = args().collect();
+        //println!("dentro de Storage, {}", args);
+
+        //let dir = std::env::var("DIR").unwrap_or_else(|_| "/default/path".to_string());
+        //let dbfilename = std::env::var("DBFILENAME").unwrap_or_else(|_| "default.db".to_string());
+
+        // let args: Vec<_> = args().collect();
+
+        let mut dir = "/default/path".to_string();
+        let mut dbfilename = "default.db".to_string();
+
+        for argument in env::args() {
+            println!("{argument}");
+            match argument.as_str() {
+                "--dir" => { dir = argument },
+                "--dbfilename" => { dbfilename = argument },
+                _ => {}
+            }
+        }
+
         Storage {
             storage: HashMap::new(),
+            config: Config { dir, dbfilename },
         }
     }
 
@@ -41,6 +70,14 @@ impl Storage {
             true => None,
             false => Some(item),
         }
+    }
+
+    pub fn set_config(&mut self, config: Config) {
+        self.config = config
+    }
+
+    pub fn get_config(&self) -> &Config {
+        &self.config
     }
 }
 
