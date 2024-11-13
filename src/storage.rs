8bde1b88a4c2ebc2acf14aa56692df364e0fe36e
Nicolas Battaglia <nicobattmdq@gmail.com>
2024-11-13 11:15:45 +0100

    CLI tests (cli-test-1731492945786)

---
+++ src/storage.rs
@@ -0,0 +1,57 @@
+
+use std::collections::HashMap;
+use std::time::Instant;
+
+#[derive(Debug)]
+pub struct Item {
+    pub value: String,
+    pub created: Instant,
+    pub expires: usize,
+}
+
+pub struct Storage {
+    pub storage: HashMap<String, Item>,
+}
+
+impl Storage {
+    pub fn new() -> Self {
+        Storage {
+            storage: HashMap::new(),
+        }
+    }
+
+    pub fn set(&mut self, key: &str,  value: &str, expires: usize) {
+        let item = Item {value: String::from(value), expires, created: Instant::now()};
+        println!("Item es : {:?}", item);
+        self.storage.insert(key.to_string(), item);
+    }
+
+    pub fn get(&self, key: &str) -> Option<&Item> {
+        let item = match self.storage.get(key) {
+            Some(item) => item,
+            None => return None,
+        };
+        println!("Item es : {:?}", item);
+        let is_expired = item.expires > 0 && item.created.elapsed().as_millis() > item.expires as u128;
+        match is_expired {
+            true => None,
+            false => Some(item)
+        } 
+    }
+}
+
+impl Default for Item {
+    fn default() -> Self {
+        Item {
+            value: String::new(),
+            created: Instant::now(),
+            expires: 0,
+        }
+    }
+}
+
+impl Default for Storage {
+    fn default() -> Self {
+        Storage::new()
+    }
+}
